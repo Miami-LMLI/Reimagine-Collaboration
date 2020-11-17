0eff488089b6a119b45300a0ffbef7e96b25840d
Kyle Lierer <liererkt@gmail.com>
2020-11-17 13:29:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Envision 2040
+
+## Pre-reqs
+- [ ] NodeJS (version >=10.13.0)
+- [ ] [Yarn](https://yarnpkg.com/getting-started/install)
+- [ ] [Contentful API](https://www.contentful.com/)
+
+## Setup
+1. `git clone git@github.com:LMLI-Cohort-9/Envision-2040-Website-Gatsby.git`
+2. `cd Envision-2040-Website-Gatsby/`
+3. `yarn install`
+4. `yarn setup`
+5. `yarn run dev`
---
+++ bin/hello.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk')
+const pkg = require('../package.json')
+
+console.log(`
+
+${chalk.green('Hey there! 👋')}
+
+${chalk.yellow.bold('yarn run setup')} automates the following steps for you:
+  - creates a config file ${chalk.yellow('./.contentful.json')}
+  - imports ${chalk.green('a predefined content model')}
+
+When this is done run:
+
+${chalk.yellow(
+  'yarn run dev'
+)} to start a development environment at ${chalk.green('localhost:8000')}
+
+or
+
+${chalk.yellow(
+  'yarn run build'
+)} to create a production ready static site in ${chalk.green('./public')}
+
+For further information check the readme of the project
+(https://github.com/LMLI-Cohort-9/Envision-2040-Website-Gatsby/tree/development)
+
+`)
