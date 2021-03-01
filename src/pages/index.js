5e138db0f0278f46fcd30ab7ffa4110fdb50c63d
Kyle Lierer <liererkt@gmail.com>
2021-03-01 09:20:09 -0500

    Shortened description.

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ class Home extends React.Component {
 
     return (
       <Layout
-        description="Join the Lockheed Martin Leadership Institute as we take a deep dive into how we can prepare for what the next 20 years will bring as we explore the intersections of technology, social rights, and what it means to be human."
+        description="Join the Lockheed Martin Leadership Institute as we take a deep dive into how we can prepare for what the next 20 years will bring."
         location={this.props.location}>
         <div className="content">
           <Header image={headerImg} alt={"Envision 2040"} applyGradient={true} />
