96a48e7964905dfce145c3dc94eee1a9b99cd3aa
Sam Kuhbander <kuhbansc@miamioh.edu>
2022-02-16 17:13:30 -0500

    added virtual schedule without speaker photos

--- src/pages/livestream.js
+++ src/pages/livestream.js
@@ -6,6 +6,13 @@ import { Col, Row } from 'react-bootstrap';
 import MemberPreview from '../components/member-preview';
 import Header from '../components/header';
 import Layout from '../components/layout';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 /**
  * The class that represents the categories page.
@@ -26,7 +33,6 @@ class Categories extends React.Component {
             <Layout
                 title="Livestream"
                 location={this.props.location}>
-
                 <div className="content">
                     <div className="wrapper">
                         <h2 className="section-headline">
@@ -34,13 +40,55 @@ class Categories extends React.Component {
                         </h2>
                     </div>
                 </div>
-                <div className="break" />
-                <div className="content" style={{textAlign:"center"}}>
+                <div className="content" style={{ textAlign: "center" }}>
                     <div className="wrapper">
                         <iframe width="960" height="540" src="https://www.youtube.com/embed/U9bAh-W2r_U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
-
+                <div>
+                    <div className="content">
+                        <div className="wrapper">
+                            <h2 className="section-headline">
+                                Virtual Schedule
+                            </h2>
+                        </div>
+                    </div>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>10:00 - 10:20 </TableCell>
+                                    <TableCell >Opening Remarks
+                                        Louise Morman - Executive Director of Lockheed Martin Leadership Institute
+                                        Dr. Gregory Crawford - President of Miami University</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>10:20 - 11:05 </TableCell>
+                                    <TableCell> Jeff Wilcox
+                                        Former Vice President of Digital Transformation at Lockheed Martin
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>12:45 - 1:30 </TableCell>
+                                    <TableCell> Amy Leschke-Kahle
+                                        Vice President of Performance Acceleration at the Marcus Buckingham Company
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>1:45 - 2:30 </TableCell>
+                                    <TableCell> Bryce Williams
+                                        Advisor + Workforce Collaboration at Eli Lilly and Company
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>2:30 - 3:00 </TableCell>
+                                    <TableCell> Closing Remarks
+                                        Dr. Beena Sukumaran - Dean, College of Engineering and Computing</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
             </Layout>
         );
     }
